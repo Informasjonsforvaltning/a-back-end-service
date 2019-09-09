59a8aa6540aafbd1e1d9603d85652be026f5d6b2
Bjørn Grøva <bjorn.grova@brreg.no>
2019-09-09 13:12:33 +0200

    pipeline definition

--- src/main/java/no/template/Application.java
+++ src/main/java/no/template/Application.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 @SpringBootApplication
 public class Application {
 
-    //force build 789t578589 3466634
+    //force build 789t578589 3466634 897897
 
     private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
