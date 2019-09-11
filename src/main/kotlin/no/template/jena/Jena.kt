f90bb32429e7e503666e94c1570125083c97e70d
Frode Roxrud Gill <extfrg@brreg.no>
2019-09-11 16:48:12 +0200

    Removed unused stuff

--- src/main/kotlin/no/template/jena/Jena.kt
+++ src/main/kotlin/no/template/jena/Jena.kt
@@ -1,40 +1,9 @@
 package no.template.jena
 
-import no.template.generated.model.TemplateObject
 import org.apache.jena.rdf.model.Model
-import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.rdf.model.Property
 import org.apache.jena.rdf.model.Resource
-import org.apache.jena.sparql.vocabulary.FOAF
-import org.apache.jena.vocabulary.DCTerms
-import org.apache.jena.vocabulary.RDF
-import org.apache.jena.vocabulary.ROV
 import java.io.ByteArrayOutputStream
-
-fun TemplateObject.jenaResponse(acceptHeader: String?): String =
-    listOf(this)
-        .createModel()
-        .createResponseString(acceptHeaderToJenaType(acceptHeader))
-
-fun List<TemplateObject>.jenaResponse(acceptHeader: String?): String =
-    createModel()
-        .createResponseString(acceptHeaderToJenaType(acceptHeader))
-
-private fun List<TemplateObject>.createModel(): Model {
-    val model = ModelFactory.createDefaultModel()
-    model.setNsPrefix("dct", DCTerms.getURI())
-    model.setNsPrefix("rov", ROV.getURI())
-
-    forEach {
-        model.createResource(it.uri)
-            .addProperty(RDF.type, FOAF.Organization)
-            .addProperty(DCTerms.identifier, it.id)
-            .safeAddProperty(ROV.legalName, it.name)
-            .safeAddLinkedProperty(DCTerms.isPartOf, it.uri)
-    }
-
-    return model
-}
 
 private fun Resource.safeAddProperty(property: Property, value: String?): Resource =
     if(value == null) this
