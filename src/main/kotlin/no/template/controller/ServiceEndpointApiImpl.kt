ea1884daea79bbab05d4ecfe15a34df6fa28dfbd
NilsOveTen <nils.ove.tendenes@acando.no>
2019-09-19 13:45:30 +0200

    Enabled authorization for create

--- src/main/kotlin/no/template/controller/ServiceEndpointApiImpl.kt
+++ src/main/kotlin/no/template/controller/ServiceEndpointApiImpl.kt
@@ -2,6 +2,7 @@ package no.template.controller
 
 import no.template.generated.model.ServiceEndpointCollection
 import no.template.generated.model.ServiceEndpoint
+import no.template.security.EndpointPermissions
 import no.template.service.ServiceEndpointService
 import org.slf4j.LoggerFactory
 import javax.servlet.http.HttpServletRequest
@@ -17,31 +18,35 @@ private val LOGGER = LoggerFactory.getLogger(ServiceEndpointApiImpl::class.java)
 
 @Controller
 open class ServiceEndpointApiImpl (
-  private val serviceEndpointService: ServiceEndpointService
+  private val serviceEndpointService: ServiceEndpointService,
+  private val endpointPermissions: EndpointPermissions
 ) : no.template.generated.api.ServiceEndpointApi {
 
-  override fun getServiceEndpoints(httpServletRequest: HttpServletRequest?): ResponseEntity<ServiceEndpointCollection> =
-    ServiceEndpointCollection()
-      .apply { total = 0 }
-      .let { response -> ResponseEntity(response, HttpStatus.OK) }
+  override fun getServiceEndpoints(httpServletRequest: HttpServletRequest): ResponseEntity<ServiceEndpointCollection> =
+      ServiceEndpointCollection()
+        .apply { total = 0 }
+	    .let { response -> ResponseEntity(response, HttpStatus.OK) }
 
   override fun createServiceEndpoint(httpServletRequest: HttpServletRequest, serviceEndpoint: ServiceEndpoint): ResponseEntity<Void> =
-    try {
-      HttpHeaders()
-        .apply {
-          location = ServletUriComponentsBuilder
-            .fromCurrentServletMapping()
-            .path("/serviceendpoint/{id}")
-            .build()
-            .expand(serviceEndpointService.createServiceEndpoint(serviceEndpoint).id)
-            .toUri() }
-        .let { ResponseEntity(it, HttpStatus.CREATED) }
-    } catch (exception: Exception) {
-      LOGGER.error("createServiceEndpoint failed:", exception)
-      when(exception) {
-        is ConstraintViolationException -> ResponseEntity(HttpStatus.BAD_REQUEST)
-        is DuplicateKeyException -> ResponseEntity(HttpStatus.CONFLICT)
-        else -> ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
-      }
-    }
+      if (endpointPermissions.hasAdminPermission()) {
+        try {
+          HttpHeaders()
+              .apply {
+                location = ServletUriComponentsBuilder
+                    .fromCurrentServletMapping()
+                    .path("/serviceendpoint/{id}")
+                    .build()
+                    .expand(serviceEndpointService.createServiceEndpoint(serviceEndpoint).id)
+                    .toUri()
+              }
+              .let { ResponseEntity<Void>(it, HttpStatus.CREATED) }
+        } catch (exception: Exception) {
+          LOGGER.error("createServiceEndpoint failed:", exception)
+          when (exception) {
+            is ConstraintViolationException -> ResponseEntity<Void>(HttpStatus.BAD_REQUEST)
+            is DuplicateKeyException -> ResponseEntity(HttpStatus.CONFLICT)
+            else -> ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+          }
+        }
+      } else ResponseEntity(HttpStatus.UNAUTHORIZED)
 }
