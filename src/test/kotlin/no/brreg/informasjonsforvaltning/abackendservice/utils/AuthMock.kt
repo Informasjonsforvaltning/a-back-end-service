fa976e344524c285c888e749485d22b34e4b76ee
rannveigskjerve <rask@netlight.com>
2019-10-28 08:23:21 +0100

    authmock; jwk and jwt generation

--- src/test/kotlin/no/brreg/informasjonsforvaltning/abackendservice/utils/AuthMock.kt
+++ src/test/kotlin/no/brreg/informasjonsforvaltning/abackendservice/utils/AuthMock.kt
@@ -2,28 +2,18 @@ package no.brreg.informasjonsforvaltning.abackendservice.utils
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import com.github.tomakehurst.wiremock.client.WireMock
+import no.brreg.informasjonsforvaltning.abackendservice.utils.jwk.JwkStore
 
 
 
+private val port = LOCAL_SERVER_PORT
+private val mockserver = WireMockServer(port)
 
 
-    private val port = LOCAL_SERVER_PORT
-    private val mockserver = WireMockServer(port)
+fun startMockAuth (){
 
-    fun startMockAuth (){
         mockserver.stubFor(get(urlEqualTo("/auth/realms/fdk/protocol/openid-connect/certs"))
-                .willReturn(okJson("{\n" +
-                        " \"keys\": [\n" +
-                        "   {\n" +
-                        "     \"kid\": \"MfFp7IWWRkFW3Yvhb1eVrtyQQNYqk6BG-6HZFpl_JxI\",\n" +
-                        "     \"kty\": \"RSA\",\n" +
-                        "     \"alg\": \"RS256\",\n" +
-                        "     \"use\": \"sig\",\n" +
-                        "     \"n\": \"qDWXUhNtfuHNh0lm3o-oTnP5S8ENpzsyi-dGrjSeewxV6GNiKTW5INJ4hDQ7ZWkUFfJJhfhQWJofqgN9rUBQgbRxXuUvEkrzXQiT9AT_8r-2XLMwRV3eV_t-WRIJhVWsm9CHS2gzbqbNP8HFoB_ZaEt2FYegQSoAFC1EXMioarQbFs7wFNEs1sn1di2xAjoy0rFrqf_UcYFNPlUhu7FiyhRrnoctAuQepV3B9_YQpFVoiUqa_p5THcDMaUIFXZmGXNftf1zlepbscaeoCqtiWTZLQHNuYKG4haFuJE4t19YhAZkPiqnatOUJv5ummc6i6CD69Mm9xAzYyMQUEvJuFw\",\n" +
-                        "     \"e\": \"AQAB\"\n" +
-                        "   }\n" +
-                        " ]\n" +
-                        "}"))
+                .willReturn(okJson(JwkStore.get() as String))
         )
 
         mockserver.stubFor(get(urlEqualTo("/ping"))
